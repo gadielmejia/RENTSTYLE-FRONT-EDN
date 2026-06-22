cf1402cb343ebc0ff8f9d116dae43dd66c4c5320
ProfeAlbeiro <profealbeiro2020@gmail.com>
2026-06-22 15:45:09 -0500

    Seguridad, cerrar sesion, y a la hora de devolver la pagina, no permite ir al dashboard que estaba con el inicio de sesion

--- src/components/NavAdmin.jsx
+++ src/components/NavAdmin.jsx
@@ -4,12 +4,16 @@ import Header from "./Header";
 function NavAdmin() {
   return (
     <nav className="app-nav">
-      <div className="nav-inner">
-        <Link to="/" className="brand">
-          <Header />
-        </Link>
+        <div className="nav-inner">
+          <Link to="" className="brand">RentStyle</Link>
+        
         <div className="nav-actions">
-          <button onClick={() => {}}>Tema</button>
+          <button onClick={() => {}}>Temaaaaaaaaaaa</button>
+          <button onClick={() => {}}>Gestion de productos</button>
+          <button onClick={() => {}}>Gestion de usuarios</button>
+          <button onClick={() => {}}>Inventario</button>
+          <button onClick={logout}>Cerrar sesióoooooooon</button>
+
 
         </div>
       </div>
